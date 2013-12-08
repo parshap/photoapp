c46f930286d2b7c9932943f0f50b2970a9a92707
jordangarcia <jordan.garcia@gmail.com>
2013-12-08 13:30:17 -0800

    Refactored into more discrete modules

--- index.js
+++ index.js
@@ -1,102 +1,88 @@
-var RESIZE_WIDTH_THRESHOLD = 1500;
-var RESIZE_HEIGHT_THRESHOLD = 1500;
-
+var _ = require('underscore')
 var fs = require('fs');
 var path = require('path');
-var gm = require('gm');
-var through = require('through');
 var async = require('async');
+var copyFile = require('./lib/copy-file');
+var compressAndMove = require('./lib/compress-and-move');
 var scaleSize = require('./lib/sizescaler.js');
+var imagesize = require('imagesize');
 
-function start(watchDir, compressedDir, originalsDir) {
-	function shouldHandle(filename) {
-		return /\.(?:jpg|jpeg|png)$/i.test(filename);
+function shouldHandle(filename) {
+	return /\.(?:jpg|jpeg|png)$/i.test(filename);
+}
+
+var DEFAULTS = {
+	outputSize: 1200,
+	minOutputSize: 960,
+	resizeWidthThreshold: 1500,
+	resizeHeightThreshold: 1500,
+};
+
+/**
+ * [opts.watchDir]
+ * [opts.compressedDir]
+ * [opts.originalsDir]
+ * [opts.outputSize]
+ * [opts.minOuputSize]
+ * [opts.resizeWidthThreshold]
+ * [opts.resizeHeightThreshold]
+ *
+ * @param {Object} opts
+ */
+function start(opts, callback) {
+	opts = _.extend(DEFAULTS, opts || {});
+
+	/**
+	 * Whether the image should be resized
+	 *
+	 * @param {Object} size
+	 */
+	function shouldResize(size) {
+		return (size.width > opts.resizeWidthThreshold || size.height > opts.resizeHeightThreshold);
 	}
 
-	fs.watch(watchDir, function(event, filename) {
+	fs.watch(opts.watchDir, function(event, filename) {
 		console.log("change: %s file: %s", event, filename);
 		if (!shouldHandle(filename)) {
 			return;
 		}
 
-		var filepath = path.join(__dirname, watchDir, filename);
+		var filepath     = path.join(__dirname, opts.watchDir, filename);
+		var resizeStream = fs.createWriteStream(path.join(opts.compressedDir, filename));
+		var backupStream = fs.createWriteStream(path.join(opts.originalsDir, filename));
 
-		fs.stat(filepath, function(err, stats) {
-			if (err || !stats.isFile()) {
-				return;
-			}
-
-			var resizeStream = fs.createWriteStream(path.join(compressedDir, filename));
-			var backupStream = fs.createWriteStream(path.join(originalsDir, filename));
-
-			async.parallel([
-				compressAndMove.bind(null, filepath, resizeStream),
-				copyFile.bind(null, filepath, backupStream),
-			], function(err, results) {
-				if (err) {
-					console.log(err);
-					return;
-				}
-				fs.unlink(filepath);
-			});
-		});
-	});
-}
-
-
-/**
- * Whether the image should be resized
- *
- * @param {Object} size
- */
-function shouldResize(size) {
-	return (size.width > RESIZE_WIDTH_THRESHOLD || size.height > RESIZE_HEIGHT_THRESHOLD);
-}
-
-function copyFile(file, outputStream, callback) {
-	var read = fs.createReadStream(file);
-
-	read.pipe(outputStream);
-	read.on('end', function() {
-		callback(null);
-	});
-
-	outputStream.on('error', callback);
-}
-
-function compressAndMove(file, resize, callback) {
-	console.log("compressAndMove for file: %s", file);
-	resize.on('end', function() {
-		console.log('compressAndMove end');
-		callback(null);
-	});
-
-	gm(file)
-		.size({ bufferStream: true }, function(err, size) {
+		imagesize(fs.createReadStream(filepath), function(err, size) {
 			if (err) {
-				console.error('error getting size: %s', err);
+				console.error(err);
 				return;
 			}
+
+			// default compressFn to just move the file to the compressedDir
+			console.log('filepath = %s', filepath);
+			var backupFn = copyFile.bind(null, filepath, backupStream);
+			var compressFn = copyFile.bind(null, filepath, resizeStream);
 
 			if (shouldResize(size)) {
-				console.log("should resize %d x %d", size.width, size.height);
-				var newSize = scaleSize(size, 1200, 960);
-				this.resize(newSize.width, newSize.height);
+				// compress fn should actually compress the file
+				var newSize = scaleSize(size, opts.outputSize, opts.minOutputSize)
+				compressFn = compressAndMove.bind(null, {
+					file: filepath,
+					output: resizeStream,
+					height: newSize.height,
+					width: newSize.width
+				});
 			}
 
-			this.stream(function(err, stdout, stderr) {
-				if (err) callback(err);
-				stdout.pipe(resize);
-
-				stdout.on('end', function() {
-					callback(null);
-				});
-			});
+			async.parallel([
+				compressFn,
+				backupFn
+			], callback);
 		});
+	});
 }
 
-var watchDir = process.argv[2];
-var compressedDir = process.argv[3];
-var originalsDir = process.argv[4];
-
-start(watchDir, compressedDir, originalsDir);
+start({
+	watchDir: process.argv[2],
+	compressedDir: process.argv[3],
+	originalsDir: process.argv[4],
+});
